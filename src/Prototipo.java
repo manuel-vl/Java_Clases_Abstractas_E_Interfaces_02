79108a1afba6f8f4a77080b8eb7051a39a553447
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-02-05 22:06:11 -0500

    [FEATURE] Add abstract class Prototipo

---
+++ src/Prototipo.java
@@ -0,0 +1,10 @@
+public abstract class Prototipo {
+    private int valorInicial;
+    private int paso;
+
+    public abstract void reiniciarSerie();
+
+    public abstract void setValorInicial();
+
+    public abstract void siguienteValor();
+}
